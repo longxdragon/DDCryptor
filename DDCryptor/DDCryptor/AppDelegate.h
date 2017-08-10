7d9f4cb5aeca040616afadba70d3e611f6225ab4
gkoudai_xulong <longxdragon@163.com>
2017-08-10 17:24:11 +0800

    Create project

---
+++ DDCryptor/DDCryptor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDCryptor
+//
+//  Created by longxdragon on 2017/8/10.
+//  Copyright © 2017年 longxdragon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDCryptor/DDCryptor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDCryptor
+//
+//  Created by longxdragon on 2017/8/10.
+//  Copyright © 2017年 longxdragon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
